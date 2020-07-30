bb34d4c2d97cfca1349385ff777ac0037c0a2261
前田 淳 <p_maeda@soft-co.jp>
2020-07-30 12:30:41 +0900

    共通部分を UnicdeSearchLib に抜き出して、テストを書く。 あと editorconfigの整理や共通記述の抜き出し

---
+++ UnicodeSearchLib.Tests/UnicodeEnumerableTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Unicode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnicodeSearch.Tests
+{
+    [TestClass]
+    public class UnicodeEnumerableTests
+    {
+        static IEnumerable<object[]> GetEnumeratorTestData
+        {
+            get
+            {
+                yield return GetEnumeratorTestData("test",
+                    UnicodeInfo.GetCharInfo(char.ConvertToUtf32("t", 0)),
+                    UnicodeInfo.GetCharInfo(char.ConvertToUtf32("e", 0)),
+                    UnicodeInfo.GetCharInfo(char.ConvertToUtf32("s", 0)),
+                    UnicodeInfo.GetCharInfo(char.ConvertToUtf32("t", 0))
+                );
+                static object[] GetEnumeratorTestData(string Text, params UnicodeCharInfo[] Expected)
+                    => new object[] { Text, Expected };
+            }
+        }
+        [TestMethod]
+        [DynamicData(nameof(GetEnumeratorTestData))]
+        public void GetEnumeratorTest(string Text, UnicodeCharInfo[] Expected)
+        {
+            Console.WriteLine($"FROM TEXT: {Text}");
+            Console.WriteLine("EXPECTED:");
+            foreach (var e in Expected)
+                Console.WriteLine($"U+{e.CodePoint:X4}: {e.GetDisplayText()}");
+            var Actual = new UnicodeEnumerable(Text).ToArray();
+            Console.WriteLine("EXPECTED:");
+            foreach (var a in Actual)
+                Console.WriteLine($"U+{a.CodePoint:X4}: {a.GetDisplayText()}");
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+    }
+}
---
+++ UnicodeSearchLib/UnicodeEnumerable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Unicode;
+
+namespace UnicodeSearch
+{
+    public class UnicodeEnumerable : IEnumerable<UnicodeCharInfo>
+    {
+        readonly string Text;
+        public UnicodeEnumerable(string Text)
+            => this.Text = Text;
+        public IEnumerator<UnicodeCharInfo> GetEnumerator()
+        {
+            var Enumerator = StringInfo.GetTextElementEnumerator(Text);
+            while (Enumerator.MoveNext())
+            {
+                var Current = Enumerator.Current.ToString();
+                var CodePoint = char.ConvertToUtf32(Current, 0);
+                var CharInfo = UnicodeInfo.GetCharInfo(CodePoint);
+                yield return CharInfo;
+                try
+                {
+                    CodePoint = char.ConvertToUtf32(Current, 1);
+                    CharInfo = UnicodeInfo.GetCharInfo(CodePoint);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+                yield return CharInfo;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
