de0b9dc79d8e3c84116038b17fee42fdd176f96b
juner <juner@juner.net>
2020-08-05 01:20:10 +0900

    /text/{Value} の タイトルを入力文字により変更する対応

--- UnicodeSearch/Program.cs
+++ UnicodeSearch/Program.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 namespace UnicodeSearch
 {
@@ -14,7 +15,7 @@ namespace UnicodeSearch
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddHeadElementHelper();
             await builder.Build().RunAsync();
         }
     }
