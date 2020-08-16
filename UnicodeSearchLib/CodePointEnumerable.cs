e0d58756a01b065114d7dd9871bf26ab94275f35
juner <juner@juner.net>
2020-08-16 16:37:01 +0900

    UnicodeCharInfo への変換の効率化

---
+++ UnicodeSearchLib/CodePointEnumerable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace UnicodeSearch
+{
+    /// <summary>
+    /// string to CodePoint
+    /// </summary>
+    public class CodePointEnumerable : IEnumerable<int>
+    {
+        readonly string Text;
+        public CodePointEnumerable(string Text)
+            => this.Text = Text;
+        public IEnumerator<int> GetEnumerator()
+        {
+            var chars = Text.ToCharArray();
+            var Enumerator = chars.GetEnumerator();
+            while (Enumerator.MoveNext())
+            {
+                var Current = (char)Enumerator.Current;
+                if (!char.IsSurrogate(Current))
+                {
+                    yield return Current;
+                    continue;
+                }
+                if (Enumerator.MoveNext())
+                {
+                    var Current2 = (char)Enumerator.Current;
+                    var CodePoint = char.ConvertToUtf32(Current, Current2);
+                    yield return CodePoint;
+                    continue;
+                }
+                yield return Current;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
--- UnicodeSearchLib/UnicodeEnumerable.cs
+++ UnicodeSearchLib/UnicodeEnumerable.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
+using System.Linq;
 using System.Unicode;
 
 namespace UnicodeSearch
 {
+    /// <summary>
+    /// string to UnicodeCharInfo
+    /// </summary>
     public class UnicodeEnumerable : IEnumerable<UnicodeCharInfo>
     {
         readonly string Text;
@@ -13,26 +15,9 @@ namespace UnicodeSearch
             => this.Text = Text;
         public IEnumerator<UnicodeCharInfo> GetEnumerator()
         {
-            var Enumerator = StringInfo.GetTextElementEnumerator(Text);
-            while (Enumerator.MoveNext())
-            {
-                var Current = Enumerator.Current.ToString();
-                var CodePoint = char.ConvertToUtf32(Current, 0);
-                var CharInfo = UnicodeInfo.GetCharInfo(CodePoint);
-                yield return CharInfo;
-                try
-                {
-                    CodePoint = char.ConvertToUtf32(Current, 1);
-                    CharInfo = UnicodeInfo.GetCharInfo(CodePoint);
-                }
-                catch (Exception)
-                {
-                    continue;
-                }
-                yield return CharInfo;
-            }
+            foreach (var CodePoint in new CodePointEnumerable(Text))
+                yield return UnicodeInfo.GetCharInfo(CodePoint);
         }
-
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
