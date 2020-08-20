3525f426ef927005e2362e22820c99bbada99795
juner <juner@juner.net>
2020-08-21 08:54:51 +0900

    CodePointEnumerator を 別ファイルに移動してAPI周りを整理

--- UnicodeSearchLib/CodePointEnumerable.cs
+++ UnicodeSearchLib/CodePointEnumerable.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 
 namespace UnicodeSearch
 {
     /// <summary>
-    /// string to CodePoint
+    /// string to CodePoint enumerable.
     /// </summary>
     public class CodePointEnumerable : IEnumerable<int>
     {
@@ -15,45 +14,6 @@ namespace UnicodeSearch
         public IEnumerator<int> GetEnumerator() => new CodePointEnumerator(Text);
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-        struct CodePointEnumerator : IEnumerator<int>
-        {
-            public CodePointEnumerator(string Text)
-                => (this.Text, this.index) = (Text, -1);
-            int index;
-            string Text;
-            bool IsEnd => index >= Text.Length;
-            bool CurrentIsSurrogatePair => index < 0 || IsEnd ? false : char.IsSurrogate(Text[index]);
-            public int Current
-            {
-                get
-                {
-                    if (IsEnd)
-                        throw new InvalidOperationException("this enumerator is end.");
-                    var Current = Text[index];
-                    if (!CurrentIsSurrogatePair)
-                        return Current;
-                    var Current2 = Text[index + 1];
-                    return char.ConvertToUtf32(Current, Current2);
-                }
-            }
-
-            object IEnumerator.Current => Current;
-
-            public void Dispose() { }
-
-            public bool MoveNext()
-            {
-                var CurrentIsSurrogatePair = this.CurrentIsSurrogatePair;
-                index++;
-                if (CurrentIsSurrogatePair)
-                    index++;
-                return !IsEnd;
-            }
-
-            public void Reset()
-            {
-                index = -1;
-            }
-        }
+        public override string ToString() => Text;
     }
 }
---
+++ UnicodeSearchLib/CodePointEnumerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UnicodeSearch
+{
+    /// <summary>
+    /// string to CodePoint enumerator.
+    /// </summary>
+    public struct CodePointEnumerator : IEnumerator<int>
+    {
+        public CodePointEnumerator(string Text)
+            => (text, this.index) = (Text, -1);
+        int index;
+        readonly string text;
+        public int Index => index;
+        public bool IsNoStart => index < 0;
+        public bool IsEnd => !(index < text.Length);
+        public bool CurrentIsSurrogatePair => IsNoStart || IsEnd ? false : char.IsSurrogate(text, index);
+        public int Current
+        {
+            get
+            {
+                if (IsNoStart)
+                    throw new InvalidOperationException("this enumerator is no MoveNext().");
+                if (IsEnd)
+                    throw new InvalidOperationException("this enumerator is end.");
+                return char.ConvertToUtf32(text, index);
+            }
+        }
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose() { }
+
+        public bool MoveNext()
+        {
+            var CurrentIsSurrogatePair = this.CurrentIsSurrogatePair;
+            index++;
+            if (CurrentIsSurrogatePair)
+                index++;
+            return !IsEnd;
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+    }
+}
